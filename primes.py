19944b085da37309309e414c1b89111e97e67c53
Kwan Yui Chiu <kwan.chiu@kcl.ac.uk>
2022-09-29 00:34:55 +0100

    modify primes function to primes.py and add .idea directory to ,gitignore

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
+    counter = 2
+    while len(list) < number_of_primes:
+        # check if a number is prime
+        isPrime = True
+        j = 0
+        while isPrime and j < len(list):
+            if counter % list[j]== 0:
+                isPrime = False
+            j += 1
+
+        if isPrime:
+            list.append(counter)
+        counter += 1
     return list
